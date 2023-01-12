5506aced3d2f94996e60030886e3f446654ce574
Rodrigo Nascimento <rodrigowaltmann.n@gmail.com>
2023-01-12 14:00:58 -0300

    Streams no Node.js - Criando middleware de JSON

--- src/server.js
+++ src/server.js
@@ -1,31 +1,18 @@
 //const http = require('http')  padrão commonJS =>  require
 import http from 'node:http' //EsModule => import/export
+import { json } from './middlewares/json.js'
 
 //HTTP status code
 
 const users = []
 
 const server = http.createServer(async (req, res) => {
-    const {
-        method,
-        url
-    } = req
+    const { method, url } = req
 
-    const buffers = []
-
-    for await (const chunk of req) {
-        buffers.push(chunk)
-    }
-
-    try {
-        req.body = JSON.parse(Buffer.concat(buffers).toString())
-    } catch {
-        req.body = null
-    }
+    await json(req, res)
 
     if (method === 'GET' && url === '/users') {
         return res
-            .setHeader('Content-type', 'application/json')
             .end(JSON.stringify(users)) //early return
     }
 
