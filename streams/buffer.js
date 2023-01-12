07cbaf27c0af3341c91646d3c3717fae7894eff4
Rodrigo Nascimento <rodrigowaltmann.n@gmail.com>
2023-01-12 13:51:32 -0300

    Streams no Node.js - Entendendo Buffers no Node.js

---
+++ streams/buffer.js
@@ -0,0 +1,5 @@
+//buffer = representação de um espaço de memória do computador usado para transitar dados de uma maneira performatica 
+
+const buff = Buffer.from('hello')
+
+console.log(buff.toJSON())
